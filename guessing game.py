03c1f54ec5a8611a7145ec6bd4c99fa2c2651588
Parth Paliwal <parthpaliwalhere@gmail.com>
2026-06-05 13:00:42 +0530

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,12 +1,12 @@
 import random
-print("aao sath time pass karte hai....chalo guessing game khelte hai")
+print("welcome to the guessing game")
 print()
-print("select kar")
+print("plzz select the level of difficulty")
 print("1. easy ")
 print("2. normal")
 print("3. hard")
 print()
-choi = int(input("konsa level?? : "))
+choi = int(input("level?? : "))
 print()
 score = 0
 print()
@@ -22,37 +22,41 @@ def ip():
     oi = int(input("Enter Your Guess : "))
     print()
     if oi > num:
-        print("so ahead...lagta hai bought door kr soachte ho")
+        print("so ahead...")
         print()
+        return False
     elif oi < num:
-        print("thoda kam hai....thoda bada num tha")
+        print("slightly behind...")
         print()
-
+        return False
     else:
         print("yupp,... you are correct")
         print()
         score += 1
-        print("score =",score)
+        print(f"score = {score}")
         print()
+        print("thankyouu!!")
         exit()
+        return True
 
 
 if choi == 1:
       print("4 life")
-      limit = 20
+      limit = "1 to 20"
       print(limit," limit")
-      print("score =",score)
+      print(f"score = {score}")
       print()
       ip()
       ip()
       ip()
       ip()
       print("number was ",num)
+
 elif choi == 2:
         print("3 life")
-        limit = 50
+        limit = "1 to 50"
         print(limit," limit")
-        print("score =",score)
+        print(f"score = {score}")
         print()
         ip()
         ip()
@@ -60,33 +64,15 @@ elif choi == 2:
         print("number was ",num)
 elif choi == 3:
         print("2 life")
-        limit = 100
+        limit = "1 to 100"
         print(limit," limit")
-        print("score =",score)
+        print(f"score = {score}")
         print()
         ip()
         ip()
         print("number was ",num)
 else:
-        exit()
-        print()
-        
+      print("invalid input")
+      exit()
 print("thankyouu!!")
 exit()
-       
-       
-       
-        
-
-        
-
-    
-        
-    
-    
-
-            
-        
-        
-        
-  
